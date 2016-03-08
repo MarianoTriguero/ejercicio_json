eab3f32ad920934855bcb5473819032c60b24cc5
Mariano Triguero Marquez <mariano2455@hotmail.com>
2016-03-08 02:24:57 +0100

    Desarrollo de actividades

--- ejercicio.py
+++ ejercicio.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 
 import json
-from pprint import pprint
 
 f=open("CentrosSanitarios.json","r")
 doc = f.readlines()
@@ -37,7 +36,7 @@ for tipo in tipos:
 	print "Hay " + str(acum) + " centros del tipo " + tipo
 
 #Muestra los centros de salud cuyo nombre contenga un numero determinado de palabras y empiecen por una cadena determinada
-num = int(raw_input("¿Cuantas palabras contiene el nombre del centro?: "))
+num = int(raw_input("\n¿Cuantas palabras contiene el nombre del centro?: "))
 cadena = raw_input("Introduzca una cadena por la que comience el nombre: ")
 for line in doc[1:]:
 	lineajson = json.loads(line)
@@ -46,8 +45,8 @@ for line in doc[1:]:
 		if len(nombrepartido) == num:
 			print "El centro " + lineajson["NOMBRE"] + " coincide con los criterios de busqueda."
 
-#Introduce un tipo de centro y muestra cuantos centros hay en las distintas localidades
-tipo = raw_input(" Introduzca un tipo de centro: ")
+#Introduce un tipo de centro y muestra el porcentaje de cuantos centros hay en las distintas localidades con respecto a las demas
+tipo = raw_input("\nIntroduzca un tipo de centro: ")
 ciudades = []
 acumulador = 0
 for line in doc[1:]:
@@ -57,11 +56,37 @@ for line in doc[1:]:
 		if lineajson["LOCALIDAD"] not in ciudades:
 			ciudades.append(lineajson["LOCALIDAD"])
 
-
 for ciudad in ciudades:
-	numero = 0
+	contador = 0
 	for line in doc[1:]:
 		lineajson = json.loads(line)
-		if lineajson["LOCALIDAD"] == ciudad:
-			numero = numero + 1
-	print "La localidad " + ciudad + " contiene " + str(numero) + "  centros de ese tipo"
+		if lineajson["TIPO"] == tipo and lineajson["LOCALIDAD"] == ciudad:
+			contador = contador + 1
+	print "La ciudad " + ciudad + " tiene el " + str((float(contador)*100.0)/float(acumulador)) + " por ciento de los centros de este tipo."
+
+#Elige una localidad determinada, despues selecciona el tipo y por ultimo elige uno de los centros mostrados, del cual facilitaremos
+#informacion para poder pedir cita o acceder al mismo
+localidad = raw_input("Introduzca una localidad determinada: ")
+tipos = []
+lista = []
+for line in doc[1:]:
+	lineajson = json.loads(line)
+	if lineajson["LOCALIDAD"] == localidad:
+		lista.append(lineajson)
+		if lineajson["TIPO"] not in tipos:
+			tipos.append(lineajson["TIPO"])
+acumulador = 0
+print "Selecciona uno de los siguientes tipos: "
+for tipo in tipos:
+	print str(acumulador) + ") " + tipo
+	acumulador = acumulador + 1
+seleccion = int(raw_input("Introduzca un numero: "))
+acumulador = 0
+for centro in lista:
+	if centro["TIPO"] == tipos[seleccion]:
+		print str(acumulador) + ") " + centro["NOMBRE"]
+		acumulador = acumulador + 1
+seleccion = int(raw_input("Introduzca un numero: "))
+
+print "El centro " + lista[seleccion]["NOMBRE"] + "esta situado en la localidad" + localidad
+print "Su telefono es " + str(lista[seleccion]["TELEFONO"]) + " y su direccion es " + lista[seleccion]["DIRECCION"]
